e6d61d16c5c12617a02238944f6d1ee15d34bbb1
Yiming Ren <azx7900@iCloud.com>
2019-05-05 22:23:31 +1000

    pug version 1

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 
+// set the view engine
+app.set('view engine', 'pug');
+
 // test express where the static files are kept
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,9 +25,50 @@ app.use('/',routes);
 //Displaying mainpage
 app.get('/', (req, res) => {
 
-    res.render('Mainpage');
+    res.render('index');
   });
-  
+
+//Render the rest pages
+app.get('/art', (req, res) => {
+
+    res.render('art.pug');
+});
+
+app.get('/entertainment', (req, res) => {
+
+    res.render('entertainment.pug');
+});
+
+app.get('/finance', (req, res) => {
+
+    res.render('finance.pug');
+});
+
+app.get('/politics', (req, res) => {
+
+    res.render('politics.pug');
+});
+
+app.get('/sport', (req, res) => {
+
+    res.render('sport.pug');
+});
+
+app.get('/tech', (req, res) => {
+
+    res.render('tech.pug');
+});
+
+app.get('/travel', (req, res) => {
+
+    res.render('travel.pug');
+});
+
+app.get('/weather', (req, res) => {
+
+    res.render('weather.pug');
+});
+
 //Start the server
 app.listen(PORT, function() {
     console.log(`Express serving at port ${PORT}`);
